e0e7a5a9c850471e7700f72f062ab9a1b611a76e
Alyssa Maris <alyssamarisdev@gmail.com>
2024-09-12 10:30:25 -0700

    Migration: Add initial create migration for database

--- OhanaMembers/OhanaMembers/DB/Models/Member.cs
+++ OhanaMembers/OhanaMembers/DB/Models/Member.cs
@@ -2,9 +2,16 @@
 {
     public class Member
     {
+        public Member(string name, int age, string gender = "Enby")
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Gender { get; set; }
         public int Age { get; set; }
+        public string Gender { get; set; }
     }
 }
